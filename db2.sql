b9d14acb805f332e3509e3eb91630863e6c77dc7
Antonio26203007 <130923054+Antonio26203007@users.noreply.github.com>
2023-04-16 12:19:04 -0600

    Add files via upload

---
+++ db2.sql
@@ -0,0 +1,5 @@
+CREATE DATABASE itversity_retail_db4;
+
+CREATE USER itversity_retail_user4 WITH ENCRYPTED PASSWORD 'itversity'
+
+GRANT ALL ON DATABASE itversity_retail_db4 TO itversity_retail_user4;
